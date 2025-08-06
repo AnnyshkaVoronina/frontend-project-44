5723934e9f0857d18d93d1218d7cbcbc95886d93
Anna Voronina <avoronina.work@gmail.com>
2025-08-06 11:31:22 +0100

    Игры разума

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,19 +2,35 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const randomNumber = Math.floor(Math.random() * 100);
 console.log('Question:', randomNumber);
 let userInput = prompt('Your answer:');
-const checkEven = (randomNumber, userInput) => {
-    if (randomNumber % 2 === 0 && userInput = 'yes') {
+var i = 0;
+let numberOfCorrectAnswer = 0;
+const checkEven = (randomNumber, userInput, status, numberOfCorrectAnswer) => {
+    if (randomNumber % 2 === 0 && userInput === 'yes') {
         console.log('Correct!');
+        numberOfCorrectAnswer += 1;
     }
-    if (randomNumber % 2 != 0 && userInput = 'no') {
+    if (randomNumber % 2 != 0 && userInput === 'no') {
         console.log('Correct!');
+        numberOfCorrectAnswer += 1;
     } else {
-        if (userInput = 'yes') {
+        if (userInput === 'yes') {
             console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
         } else {
             console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
         }
-        return console.log("Let's try again, Bill!");
+        status = false;
+        return console.log("Let's try again, ${name}!"), status;
     }
+    return status, numberOfCorrectAnswer;
 };
-default export checkEven;
+
+do {
+    const randomNumber = Math.floor(Math.random() * 100);
+    console.log('Question:', randomNumber);
+    let userInput = prompt('Your answer:');
+    let status = true;
+    checkEven (randomNumber, userInput, status, numberOfCorrectAnswer);
+} while (status === 'true' && i < 2);
+if (numberOfCorrectAnswer === 3) {
+    console.log ('Congratulations, ${name}!');
+}
